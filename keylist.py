82821f36983e783ca8c86dea1959b4675502443b
shinau21 <bandingandesa@gmail.com>
2023-03-26 13:39:02 +0700

    Added Keylist for List Encryption

--- keylist.py
+++ keylist.py
@@ -1,7 +1,10 @@
 import os
 
 keylist = []
+ext = []
 for p, d, f in os.walk('pub'):
     for name in f:
         keylist.append(name)
+        ext.append(name.split('.')[1])
 print(keylist)
+#print(ext)
